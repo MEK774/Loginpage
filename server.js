83363c9e728f9470d4132e5eb046fa50d7499a06
MEK774 <helloworldmek@gmail.com>
2026-02-17 11:40:32 +0700

    add html page

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("เว็บทำงานแล้ว 🚀");
-});
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running");
 });
